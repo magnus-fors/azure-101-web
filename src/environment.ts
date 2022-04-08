54d22dae5b342c1c9b7419925779bbe4066e8023
Magnus Fors <magnus.fors@axis.com>
2022-04-08 10:27:52 +0200

    Updated ci/cd workflow

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,7 @@ export interface Environment {
 }
 
 const prod: Environment = {
-  baseUrl: "http://localhost:7071",
+  baseUrl: "https://functionapp-qprppmwsyty2i.azurewebsites.net",
 }
 
 export const environment: Environment = prod
